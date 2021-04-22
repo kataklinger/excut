7296312f15b6e199a270b729ed026bca4dbd4539
kataklinger <kataklinger@gmail.com>
2021-04-22 23:02:26 +0200

    Return arrays of cutouts and bin headers

--- excut.wrap/Wrapper.cs
+++ excut.wrap/Wrapper.cs
@@ -1,15 +1,14 @@
-﻿using excut.core;
-using ExcelDna.Integration;
+﻿using ExcelDna.Integration;
+using excut.core;
 using Microsoft.FSharp.Collections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace excut.wrap {
     public static class Wrapper {
         [ExcelFunction(Description = "Generates cutting list")]
-        public static string CUTLST(int binSize, int cutSize, object data) {
+        public static dynamic CUTLST(int binSize, int cutSize, object data) {
             if (cutSize < 0) {
                 throw new ArgumentOutOfRangeException("Cut size must be zero or positive number", nameof(cutSize));
             }
@@ -34,15 +33,14 @@ namespace excut.wrap {
 
             var bins = Engine.optimize(new Engine.Options(binSize, cutSize), ListModule.OfSeq(cutouts));
 
-            var sb = new StringBuilder();
-            var no = 0;
+            var objects = new List<object[]>();
             foreach (var bin in bins) {
-                sb.Append($"Bin #{++no} ({bin.used}/{bin.cuts}/{bin.rest}): ");
-                sb.Append(string.Join(", ", bin.cutouts.Select(c => $"{c.size} ({c.label})")));
-                sb.Append(Environment.NewLine);
+                objects.Add(new object[] { bin.used, bin.cuts, bin.rest });
+                objects.Add(bin.cutouts.Select(c => (object)c.size).ToArray());
+                objects.Add(bin.cutouts.Select(c => (object)c.label).ToArray());
             }
 
-            return sb.ToString();
+            return objects.ToArray();
         }
     }
 }
